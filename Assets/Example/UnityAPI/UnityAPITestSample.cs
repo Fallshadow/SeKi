06db820f8c27df5254a14b6d669edc7ab5034498
sunshuchao <www.sunshuchao@outlook.com>
2020-12-28 10:44:12 +0800

    新增：测试代码块性能

---
+++ Assets/Example/UnityAPI/UnityAPITestSample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityAPITestSample : MonoBehaviour
+{
+    void Update()
+    {
+        TestSample();
+    }
+
+    // 相当于将这中间的代码深度分析，用来测试性能
+    public void TestSample()
+    {
+        UnityEngine.Profiling.Profiler.BeginSample("[Test]DebugLogEmptySample");
+        ASeKi.debug.PrintSystem.Log("It's empty");
+        UnityEngine.Profiling.Profiler.EndSample();
+    }
+}
