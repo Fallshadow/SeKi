00b0216e09571f0d4f21333e42ff30f734c0cc63
sunshuchao <www.sunshuchao@outlook.com>
2020-12-28 20:41:22 +0800

    新增：游戏状态机

---
+++ Assets/Scripts/FSM/Game/Entry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ASeKi.fsm
+{
+    public class Entry : State<game.GameController>
+    {
+        public override void Enter()
+        {
+            initManagers();
+        }
+
+        private void initManagers()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using ASeKi.fsm;
+using UnityEngine;
+
+namespace ASeKi.game
+{
+    public class GameController : SingletonMonoBehaviorNoDestroy<GameController>
+    {
+        private Fsm<GameController> m_fsm = new Fsm<GameController>();
+        private IEnumerator Start()
+        {
+            SetManagersActive(false);
+            yield return new WaitForEndOfFrame();   // TODO:这里应该读取本地表格数据
+            SetManagersActive(true);
+            initState();
+            m_fsm.SwitchToState((int)GameFsmState.ENTRY);
+        }
+
+        private void initState()
+        {
+            m_fsm.Initialize(this);
+            m_fsm.AddState((int)GameFsmState.ENTRY, new Entry());
+        }
+
+        // NOTE: 初始化完成前先停止GameControll上所有MonoBehaviour的Update。
+        private void SetManagersActive(bool isActive)
+        {
+            MonoBehaviour[] managers = GetComponents<MonoBehaviour>();
+            for(int i = 0; i < managers.Length; ++i)
+            {
+                managers[i].enabled = isActive;
+            }
+        }
+    }
+}
