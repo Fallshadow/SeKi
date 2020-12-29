8971fb31f53b60957233c11790ebc2ea05bee3f2
sunshuchao <www.sunshuchao@outlook.com>
2020-12-29 08:57:44 +0800

    新增：安卓与unity交互例子

---
+++ Assets/Example/Android/AndroidTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;using UnityEngine.UI;
+
+public class AndroidTest : MonoBehaviour
+{
+
+    AndroidJavaClass unity;
+    AndroidJavaObject currentActivity;
+    AndroidJavaClass phoneInfoMgr;
+    public Text wifi;
+    private void Awake()
+    {
+        Initialize();
+    }
+    private void Update()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR_WIN
+        wifi.text = GetWifi().ToString();        
+#endif
+    }
+
+    public void Initialize()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR_WIN
+            unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+            phoneInfoMgr = new AndroidJavaClass("com.ssc.fortomorrowtest.MainTest");
+            phoneInfoMgr.CallStatic("Init", currentActivity);
+#endif
+    }
+
+#if UNITY_ANDROID && !UNITY_EDITOR_WIN
+        public float GetSignal()
+        {
+            return phoneInfoMgr.CallStatic<int>("GetSignal");
+        }
+
+        public int GetWifi()
+        {
+            return phoneInfoMgr.CallStatic<int>("getWifiInfo");
+        }
+#endif
+}
---
+++ Assets/Example/UnityAPI/Editor/UnityAPITestDrawGizmo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class UnityAPITestDrawGizmo : MonoBehaviour
+{
+    [DrawGizmo(GizmoType.Selected | GizmoType.Pickable)]
+    static void DrawGizmoForThis(UnityAPITestDrawGizmoComponentTest info,GizmoType gizmoType)
+    {
+        Vector3 position = info.transform.position;
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(position, Vector3.one);
+    }
+}
