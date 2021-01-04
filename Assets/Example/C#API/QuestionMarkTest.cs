8afb3b907660ed2350eed73e3895e50717de33a0
sunshuchao <www.sunshuchao@outlook.com>
2021-01-04 14:32:00 +0800

    新增：？ ？？ 和 time api

---
+++ Assets/Example/C#API/QuestionMarkTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionMarkTest : MonoBehaviour
+{
+    // ？ 操作符表示可空数值类型，比如bool? int? 这相当于多了一个判断条件，避免使用神奇数字0之类的作为判断条件而引起的不必要问题
+    // ？？ 这个操作符可以用来给空设置默认值，用来简化判断再好不过
+    public int? x = null;
+    private void Start()
+    {
+        int y = x ?? 1000;
+    }
+}
---
+++ Assets/Example/UnityAPI/Time/UnityAPITestTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityAPITestTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"Time.time:{Time.time}");
+        Debug.Log($"Time.frameCount:{Time.frameCount}");
+    }
+}
