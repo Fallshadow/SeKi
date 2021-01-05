717108bc70ccb58d3328318682b5c22688eeb6d0
sunshuchao <www.sunshuchao@outlook.com>
2021-01-05 16:40:24 +0800

    新增：invoke测试

---
+++ Assets/Example/UnityAPI/Mono/UnityAPIMonoInvokeTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityAPIMonoInvokeTest : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    public void TestInvoke(UnityAPIMonoInvokeTestInput unityAPIMonoInvokeTestInput)
+    {
+        unityAPIMonoInvokeTestInput.Invoke("DebugLogSthing", 2);
+    }
+
+    public void TestInvoke()
+    {
+        Invoke("DebugLogSthing", 2);
+    }
+
+    void DebugLogSthing()
+    {
+        Debug.Log("执行了");
+    }
+}
---
+++ Assets/Example/UnityAPI/Mono/UnityAPIMonoInvokeTestInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityAPIMonoInvokeTestInput : MonoBehaviour
+{
+    public UnityAPIMonoInvokeTest invokeTest;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            invokeTest.TestInvoke();
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            invokeTest.TestInvoke(this);
+        }
+    }
+}
