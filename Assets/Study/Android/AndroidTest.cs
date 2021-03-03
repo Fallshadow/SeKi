6a067db3ae4d36aac8df2a30cb2de30eb1bcdcd6
Fallshadow <www.sunshuchao@outlook.com>
2021-03-03 21:21:15 +0800

    update:安卓与unity交互

--- Assets/Study/Android/AndroidTest.cs
+++ Assets/Study/Android/AndroidTest.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Android;
 
 public class AndroidTest : MonoBehaviour
 {
@@ -9,14 +11,23 @@ public class AndroidTest : MonoBehaviour
     AndroidJavaObject currentActivity;
     AndroidJavaClass phoneInfoMgr;
     public Text wifi;
+    public Button request;
+    public Button call;
     private void Awake()
     {
         Initialize();
     }
+    private void Start()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR_WIN
+        request.onClick.AddListener(CallPhoneRequest);
+        call.onClick.AddListener(CallPhone);
+#endif
+    }
     private void Update()
     {
 #if UNITY_ANDROID && !UNITY_EDITOR_WIN
-        wifi.text = GetWifi().ToString();        
+        // wifi.text = GetWifi().ToString();        
 #endif
     }
 
@@ -25,7 +36,7 @@ public class AndroidTest : MonoBehaviour
 #if UNITY_ANDROID && !UNITY_EDITOR_WIN
             unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
             currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-            phoneInfoMgr = new AndroidJavaClass("com.ssc.fortomorrowtest.MainTest");
+            phoneInfoMgr = new AndroidJavaClass("com.example.phoneinfohelper.PhoneInfoMgr");
             phoneInfoMgr.CallStatic("Init", currentActivity);
 #endif
     }
@@ -39,6 +50,19 @@ public class AndroidTest : MonoBehaviour
         public int GetWifi()
         {
             return phoneInfoMgr.CallStatic<int>("getWifiInfo");
+        }
+
+        public void CallPhoneRequest()
+        {
+            if(!Permission.HasUserAuthorizedPermission("android.permission.CALL_PHONE"))
+            {
+                Permission.RequestUserPermission("android.permission.CALL_PHONE");
+            }
+        }
+
+        public void CallPhone()
+        {
+            phoneInfoMgr.CallStatic("CallPhone", "18846047017");
         }
 #endif
 }
