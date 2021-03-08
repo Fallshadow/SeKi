9c0a4610e5402a9ecd38234c6811f22a0733f4ac
sunshuchao <www.sunshuchao@outlook.com>
2021-03-08 20:01:10 +0800

    新增：playable实验体

---
+++ Assets/Study/Playable/TestPlayable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TestPlayable : MonoBehaviour
+{
+    private PlayableGraph playableGraph;
+    
+    private void Start()
+    {
+        playableGraph = PlayableGraph.Create();
+        
+    }
+}
