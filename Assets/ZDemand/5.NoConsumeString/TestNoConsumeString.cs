77c4d641e69d00b39dc48206cde821bfd62247de
sunshuchao <www.sunshuchao@outlook.com>
2021-03-08 10:29:18 +0800

    add:C# 字符串GC

---
+++ Assets/ZDemand/5.NoConsumeString/TestNoConsumeString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+public class TestNoConsumeString : MonoBehaviour
+{
+    public string TestStr = "";
+
+    public byte[] bytes = new Byte[8];
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            TestStr = "我去";
+        }
+        
+        if (Input.GetKey(KeyCode.B))
+        {
+            string newString = "我去";
+            newString = "哈哈";
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            TestStr = Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
