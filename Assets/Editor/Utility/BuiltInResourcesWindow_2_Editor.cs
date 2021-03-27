4729de121e84d9a3e0448b41b640c85781a0786b
sunshuchao <www.sunshuchao@outlook.com>
2021-03-27 16:22:03 +0800

    update:另一种查看内置图标的方法

---
+++ Assets/Editor/Utility/BuiltInResourcesWindow_2_Editor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BuiltInResourcesWindow_2_Mono))] 
+public class BuiltInResourcesWindow_2_Editor : Editor 
+{
+    public override void OnInspectorGUI() 
+    {
+        BuiltInResourcesWindow_2_Mono test = (BuiltInResourcesWindow_2_Mono) target;
+        string a;
+        for (int i = 0; i < test.texure.Count; i++) {
+            if (test.texure [i]) {
+                a  = test.texure [i].name;
+            } else
+                a = "";
+            test.texure[i]=EditorGUILayout.ObjectField(i+":"+a,test.texure[i],typeof(Texture),true) as Texture;
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/BuiltInResourcesWindow_2_Mono.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class BuiltInResourcesWindow_2_Mono : MonoBehaviour
+{
+    [SerializeField] 
+    public static List<string> texName = new List<string> ();
+    [SerializeField] 
+    public List<Texture> texure=new List<Texture>();
+    public List<string> name = new List<string> ();
+    void OnEnable ()
+    {
+#if UNITY_EDITOR
+        name=texName;
+        for (int i = 0; i < texName.Count; i++) {
+            texure.Add(EditorGUIUtility.Load (texName[i]) as Texture);
+        }
+#endif
+    }
+
+
+    [InitializeOnLoadMethod]
+    static void GetBultinAssetNames ()
+    {
+        var flags = BindingFlags.Static | BindingFlags.NonPublic;
+        var info = typeof(EditorGUIUtility).GetMethod ("GetEditorAssetBundle", flags);
+        var bundle = info.Invoke (null, new object[0]) as AssetBundle;
+        foreach (var n in bundle.GetAllAssetNames()) {
+            if (n.IndexOf(".png")>=0) {
+                texName.Add (n);
+            }
+            Debug.Log (n);
+        }
+    }
+}
