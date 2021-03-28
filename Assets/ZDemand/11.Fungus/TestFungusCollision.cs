ff436c728d70be8ca35611b2fe4ad8859675150c
Fallshadow <www.sunshuchao@outlook.com>
2021-03-29 00:16:41 +0800

    fungus

---
+++ Assets/ZDemand/11.Fungus/TestFungusCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Fungus;
+using UnityEngine;
+using Collision = Fungus.Collision;
+
+public class TestFungusCollision : MonoBehaviour
+{
+    public Flowchart MyFlowchart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyFlowchart.GetVariable("Var1") = GetComponent<Collision>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
