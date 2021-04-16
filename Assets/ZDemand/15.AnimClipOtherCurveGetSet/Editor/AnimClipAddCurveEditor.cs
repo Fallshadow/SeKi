c48fb870ffdd20c9c048a42c61364b59d807b494
sunshuchao <www.sunshuchao@outlook.com>
2021-04-16 10:38:11 +0800

    update:更新15

--- Assets/ZDemand/15.AnimClipOtherCurveGetSet/Editor/AnimClipAddCurveEditor.cs
+++ Assets/ZDemand/15.AnimClipOtherCurveGetSet/Editor/AnimClipAddCurveEditor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -7,6 +8,8 @@ using UnityEditor.UIElements;
 public class AnimClipAddCurveEditor : EditorWindow
 {
     private AnimationClip curAnimationClip = null;
+    private List<AnimationCurve> curves = new List<AnimationCurve>();
+    private List<EditorCurveBinding> bindings = new List<EditorCurveBinding>();
     
     [MenuItem("Tests/自制编辑器/动画文件加入IK等曲线工具")]
     public static void ShowExample()
@@ -17,13 +20,14 @@ public class AnimClipAddCurveEditor : EditorWindow
 
     public void OnEnable()
     {
+        // 初始化
         VisualElement root = rootVisualElement;
         VisualTreeAsset visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/ZDemand/15.AnimClipOtherCurveGetSet/Editor/AnimClipAddCurveEditor.uxml");
         VisualElement labelFromUXML = visualTree.CloneTree();
         root.Add(labelFromUXML);
         StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/ZDemand/15.AnimClipOtherCurveGetSet/Editor/AnimClipAddCurveEditor.uss");
         root.styleSheets.Add(styleSheet);
-
+        // 动画文件塞入
         ObjectField animationClip = root.Q<ObjectField>("ObjectField-AnimationClip");
         curAnimationClip = animationClip.value as AnimationClip;
         animationClip.objectType = typeof(AnimationClip);
@@ -34,19 +38,70 @@ public class AnimClipAddCurveEditor : EditorWindow
             if (obj is AnimationClip)
             {
                 curAnimationClip = obj as AnimationClip;
+                refreshCurves(curAnimationClip);
             }
             else
             {
                 curAnimationClip = null;
-                Debug.Log("当前动画文件为空！");
+                Debug.LogError("当前动画文件为空！");
             }
         });
-        
+        // imgui动画文件控制
         IMGUIContainer curvesImguiContainer = root.Q<IMGUIContainer>("IMGUIContainer-CurveWindow");
         curvesImguiContainer.onGUIHandler = () =>
         {
             // AnimationCurve testCurve = EditorGUILayout.CurveField("", new AnimationCurve());
+            EditorGUILayout.BeginVertical();
+            
+            for (int index = 0; index < bindings.Count; index++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                
+                EditorGUILayout.LabelField(bindings[index].propertyName);
+                curves[index] = EditorGUILayout.CurveField(curves[index]);
+                
+                EditorGUILayout.EndHorizontal();
+            }
 
+            EditorGUILayout.EndVertical();
         };
+        // 保存按钮
+        Button saveBtn = root.Q<Button>("Button-Save-Clip");
+        saveBtn.clicked += writeBackToAnimationClip;
+    }
+
+    private void refreshCurves(AnimationClip clip)
+    {
+        if (curAnimationClip == null)
+        {
+            Debug.LogError("当前动画文件为空！");
+            return;
+        }
+        Undo.RecordObject(clip,"Clip");
+        bindings.Clear();
+        curves.Clear();
+        EditorCurveBinding[] curveBindings = AnimationUtility.GetCurveBindings(clip);
+        foreach (var curveBinding in curveBindings)
+        {
+            bindings.Add(curveBinding);
+            curves.Add(AnimationUtility.GetEditorCurve(clip, curveBinding));
+        }
+    }
+
+    private void writeBackToAnimationClip()
+    {
+        if (curAnimationClip == null)
+        {
+            Debug.LogError("当前动画文件为空！");
+            return;
+        }
+        
+        for (int index = 0; index < bindings.Count; index++)
+        {
+            AnimationUtility.SetEditorCurve(curAnimationClip, bindings[index], curves[index]);
+        }
+        
+        EditorUtility.SetDirty(curAnimationClip);
+        AssetDatabase.SaveAssets();
     }
 }
