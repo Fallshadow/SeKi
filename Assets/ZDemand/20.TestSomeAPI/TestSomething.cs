185745cf8508c466028575909c848258d47ea7de
sunshuchao <www.sunshuchao@outlook.com>
2021-05-14 11:13:51 +0800

    add : 20 21 22 测试api 截图工具 前后左右

---
+++ Assets/ZDemand/20.TestSomeAPI/TestSomething.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using BindingFlags = System.Reflection.BindingFlags;
+
+public static class TestSomething
+{
+    [MenuItem("Tests/一些代码测试/C#Api/File.Copy")]
+    public static void CSharpFileCopy()
+    {
+        //string pathSrcPathFolder = Application.dataPath + "/../" + "/TestFileCopy";
+        //string pathTargetPathFolder = Application.dataPath + "/../" + "/TestFileCopy" + "/Test.txt";
+        
+        string pathSrcPathFolder = Application.dataPath + "/TestFileCopy";
+        string pathTargetPathFolder = Application.dataPath + "/TestFileCopy";
+        
+        Debug.Log($"Application.dataPath { Application.dataPath }");
+        Debug.Log($"pathSrcPathFolder { pathSrcPathFolder }");
+        Debug.Log($"pathTargetPathFolder { pathTargetPathFolder }");
+        
+        foreach (var file in Directory.GetFiles (pathSrcPathFolder))
+        {
+            string pathTargetPath = pathTargetPathFolder + "/" + file.Substring(pathSrcPathFolder.Length + 1);
+            
+            Debug.Log($"file.Length : {file.Length} file.Substring : {file.Substring (pathSrcPathFolder.Length + 1)}");
+            Debug.Log($"pathTargetPath { pathTargetPath }");
+            
+            if (File.Exists(pathTargetPath))
+            {
+                Debug.Log($"{File.Exists("F:\\SeKi\\SeKi\\Assets\\TestFileCopy\\Test.txt")}");
+                File.Copy("F:/SeKi/SeKi/TestFileCopy/Test.txt",  "F:/SeKi/SeKi/Assets/TestFileCopy/Test.txt", true);
+            }
+            else
+            {
+                File.Copy(file,  pathTargetPath);
+            }
+        }
+        
+        Debug.Log($"总结：" +
+                  $"File.Copy 相同路径文件会报错 " +
+                  $"\\..\\这种可以" +
+                  $"正反斜杠没区别");
+    }
+    
+    [MenuItem("Tests/一些代码测试/C#Api/File.Exists")]
+    public static void CSharpFileExists()
+    {
+        string path = $"{Application.dataPath}/../TestFileCopy/Test.txt";
+        Debug.Log($"TestFileCopy {path} { File.Exists(path)}");
+        
+    }
+}
---
+++ Assets/ZDemand/22.FrontBackRightLeft/TestJudgeDir.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestJudgeDir : MonoBehaviour
+{
+    public Transform My = null;
+    public Transform Other = null;
+    void Update()
+    {
+        var position = My.position;
+        var position1 = Other.position;
+        Vector3 dir = new Vector3(position.x - position1.x, 0 ,position.z - position1.z);
+        float angletmp = Vector3.SignedAngle(dir, My.forward, Vector3.up);
+        Debug.Log(angletmp);
+    }
+}
